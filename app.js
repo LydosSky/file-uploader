a02cc43132094aa4e3c4c8ff9841b8776cb3bcc9
Lydos <lydossky@gmail.com>
2025-02-10 20:57:10 +0300

    chore: basic setup

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('node:path');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const passport = require('./passport');
+const session = require('express-session');
+dotenv.config();
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+// Authentication
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+  console.log(`Express app listenning on port ${PORT}!`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
