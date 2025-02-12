2b55c87229dbda86e376616d460341b5dbda3fee
Lydos <lydossky@gmail.com>
2025-02-12 23:26:28 +0300

    feat: user model crud

---
+++ models/User.js
@@ -0,0 +1,11 @@
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+// CRUD for user NO update not necessary there is nothing to update
+exports.createUser = (user) => prisma.user.create({ data: user });
+exports.getUserById = (id) => prisma.user.findUnique({ where: { id } });
+exports.getUserByUsername = (username) =>
+  prisma.user.findUnique({ where: { username } });
+exports.getUsers = () => prisma.user.findMany();
+exports.deleteUser = (id) => prisma.user.delete({ where: { id } });
