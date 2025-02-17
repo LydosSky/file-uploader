139f221cd3cbc062f2ab3b79a11048822c2f49f5
Lydos <lydossky@gmail.com>
2025-02-18 00:19:24 +0300

    feat: login view and controller serving

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('node:path');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-// const passport = require('./passport');
+const passport = require('./passport');
 const session = require('express-session');
 dotenv.config();
 const authenticationRouter = require('./routes/authenticationRouter');
--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -1,20 +1,21 @@
 const asyncHandler = require('express-async-handler');
 
 exports.getUserSignUP = asyncHandler(function (req, res) {
-  res.render('sign-up', { title: 'Signup' });
+  res.render('sign-up', { title: 'SignUp' });
 });
+
 exports.getUserLogIn = asyncHandler(function (req, res) {
-  res.send('USER get log-in');
+  res.render('log-in', { title: 'LogIn' });
 });
-exports.getUserLogOut = asyncHandler(function (req, res) {
-  res.send('USER get log-out');
-});
+
 exports.postUserSignUp = asyncHandler(function (req, res) {
   res.send('USER post sign-up');
 });
+
 exports.postUserLogIn = asyncHandler(function (req, res) {
   res.send('USER post log-in');
 });
+
 exports.postUserLogOut = asyncHandler(function (req, res) {
   res.send('USER post log-out');
 });
