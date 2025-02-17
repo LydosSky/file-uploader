797544e5abbe16dbf6777d824138c6a027fcf636
Lydos <lydossky@gmail.com>
2025-02-18 00:14:56 +0300

    feat: express session with prisma-session

--- app.js
+++ app.js
@@ -6,23 +6,39 @@ const morgan = require('morgan');
 const session = require('express-session');
 dotenv.config();
 const authenticationRouter = require('./routes/authenticationRouter');
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
+const { PrismaClient } = require('@prisma/client');
 
 const app = express();
-
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 // Authentication
-// app.use(session({ secret: 'cats', resave: false, saveUninitialized: false }))
-// app.use(passport.session());
-// app.use((req, res, next) => {
-//   res.locals.currentUser = req.user;
-//   next();
-// });
+app.use(
+  session({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    },
+    secret: 'order of the phoenix',
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 2 * 60 * 1000,
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  }),
+);
+app.use(passport.session());
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('index', { title: 'Home' });
 });
 
 app.use('/auth', authenticationRouter);
