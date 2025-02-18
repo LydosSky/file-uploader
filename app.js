4b2b8fc8d80fbbc398c3c1171ab6858f151a860b
Lydos <lydossky@gmail.com>
2025-02-18 22:42:09 +0300

    feat: file model

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const morgan = require('morgan');
 const passport = require('./passport');
 const session = require('express-session');
 dotenv.config();
-const authenticationRouter = require('./routes/authenticationRouter');
 const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
 const { PrismaClient } = require('@prisma/client');
 
+const user = require('./models/User');
+const authenticationRouter = require('./routes/authenticationRouter');
 const app = express();
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -38,7 +39,12 @@ app.use((req, res, next) => {
   next();
 });
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Home' });
+  console.log(res.locals.currentUser);
+  res.render('index', { title: 'Home', user: res.locals.currentUser });
+});
+
+app.get('/users', function (req, res) {
+  user.getUsers().then((users) => res.send(users));
 });
 
 app.use('/auth', authenticationRouter);
---
+++ models/File.js
@@ -0,0 +1,8 @@
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+exports.createFile = (file) => prisma.file.create({ data: file });
+exports.getFiles = () => prisma.file.findMany();
+exports.getFileById = (id) => prisma.file.findFirst({ where: { id } });
+exports.deleteFile = (id) => prisma.file.delete({ where: { id } });
