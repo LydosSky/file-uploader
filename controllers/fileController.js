c5d87d433f300090d4ca3272f362a89af6bf1681
Lydos <lydossky@gmail.com>
2025-02-25 22:46:30 +0300

    feat: file upload with multer functional

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,6 +1,44 @@
+const { response } = require('express');
 const file = require('../models/File');
 const asyncHandler = require('express-async-handler');
 
 exports.getFileAdd = asyncHandler((req, res) =>
-  res.render('file-create-form', { title: 'Add File' }),
+  res.render('add-file-form', { title: 'Add File' }),
+);
+
+exports.postFileAdd = asyncHandler((req, res) =>
+  file
+    .createFile({
+      name: req.file.filename,
+      path: req.file.path,
+      storageId: parseInt(req.user.storage.id),
+      details: {
+        encoding: req.file.encoding,
+        size: req.file.size,
+        mimetype: req.file.mimetype,
+      },
+    })
+    .then(() => res.redirect('/storage')),
+);
+
+exports.getFileAddToFolder = asyncHandler((req, res) =>
+  res.render('add-file-to-folder-form', {
+    title: 'Add File',
+    folderId: req.params.folderId,
+  }),
+);
+exports.postFileAddToFolder = asyncHandler((req, res) =>
+  file
+    .createFile({
+      name: req.file.filename,
+      path: req.file.path,
+      storageId: parseInt(req.user.storage.id),
+      details: {
+        encoding: req.file.encoding,
+        size: req.file.size,
+        mimetype: req.file.mimetype,
+      },
+      folderId: parseInt(req.params.folderId),
+    })
+    .then((responsee) => res.redirect('/storage')),
 );
--- routes/fileRouter.js
+++ routes/fileRouter.js
@@ -1,7 +1,16 @@
 const { Router } = require('express');
+const upload = require('../multer');
 const fileController = require('../controllers/fileController');
+
 const router = Router();
 
-router.get('/', fileController.getFileAdd);
+router.get('/add', fileController.getFileAdd);
+router.get('/add/:folderId', fileController.getFileAddToFolder);
+router.post('/add', upload.single('file'), fileController.postFileAdd);
+router.post(
+  '/add/:folderId',
+  upload.single('file'),
+  fileController.postFileAddToFolder,
+);
 
 module.exports = router;
